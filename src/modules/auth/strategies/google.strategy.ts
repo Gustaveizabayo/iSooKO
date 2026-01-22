69e284d8a52d125379f85e9e00584d60163b6ca9
izabayo gustave <izabayogustave54@gmail.com>
2026-01-22 12:42:24 +0200

    Fix: Update GoogleStrategy to return full user entity with ID to prevent 500 error

--- src/modules/auth/strategies/google.strategy.ts
+++ src/modules/auth/strategies/google.strategy.ts
@@ -2,10 +2,14 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-    constructor(private configService: ConfigService) {
+    constructor(
+        private configService: ConfigService,
+        private authService: AuthService,
+    ) {
         super({
             clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
             clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
@@ -21,14 +25,14 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         done: VerifyCallback,
     ): Promise<any> {
         const { name, emails, photos } = profile;
-        const user = {
+        const user = await this.authService.validateGoogleUser({
             email: emails[0].value,
             firstName: name.givenName,
             lastName: name.familyName,
             picture: photos[0].value,
             accessToken,
             googleId: profile.id
-        };
+        });
         done(null, user);
     }
 }
